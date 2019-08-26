a0229c82bd6f2418f3b4d402261ef288c81a1ae8
hslam <791874158@qq.com>
2019-08-26 22:41:09 +0800

    change Conn interface

--- transporter.go
+++
@@ -1,9 +0,0 @@
-package rpc
-
-type Transporter interface {
-	Handle(readChan chan []byte,writeChan chan []byte, stopChan chan bool)
-	TickerFactor()(int)
-	BatchFactor()(int)
-	Retry()(error)
-	Close()(error)
-}
