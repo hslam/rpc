f41a5cf05c1d31cf707141956bc50d845027a735
hslam <791874158@qq.com>
2020-07-17 03:27:29 +0800

    add examples/reverseproxy

---
+++ examples/reverseproxy/server/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"github.com/hslam/rpc"
+	"github.com/hslam/rpc/encoder/codepb"
+	"github.com/hslam/rpc/encoder/pb"
+	"github.com/hslam/rpc/examples/reverseproxy/service"
+	"github.com/hslam/socket/tcp"
+)
+
+var addr string
+
+func init() {
+	flag.StringVar(&addr, "addr", ":9999", "-addr=:9999")
+	flag.Parse()
+}
+func main() {
+	rpc.Register(new(service.Arith))
+	rpc.ListenWithOptions(addr, &rpc.Options{NewSocket: tcp.NewSocket, NewCodec: pb.NewCodec, NewEncoder: codepb.NewEncoder})
+}
