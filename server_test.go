43be24fe6a3a6e82815fc600a0e6b3b4b89e3341
hslam <791874158@qq.com>
2020-09-24 19:04:43 +0800

    add server test

---
+++ server_test.go
@@ -0,0 +1,131 @@
+package rpc
+
+import (
+	"github.com/hslam/rpc/examples/codec/json/service"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestServerPoll(t *testing.T) {
+	network := "tcp"
+	addr := ":9999"
+	codec := "json"
+	server := NewServer()
+	server.SetPoll(true)
+	err := server.Register(new(service.Arith))
+	if err != nil {
+		t.Error(err)
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		server.Listen(network, addr, codec)
+	}()
+	time.Sleep(time.Millisecond * 10)
+	conn, err := Dial(network, addr, codec)
+	if err != nil {
+		t.Error(err)
+	}
+	err = conn.Ping()
+	if err != nil {
+		t.Error(err)
+	}
+
+	A := int32(4)
+	B := int32(8)
+	req := &service.ArithRequest{A: A, B: B}
+	var res service.ArithResponse
+	if err := conn.Call("Arith.Multiply", req, &res); err != nil {
+		t.Error(err)
+	}
+	if res.Pro != A*B {
+		t.Error(res.Pro)
+	}
+	conn.Close()
+	server.Close()
+	wg.Wait()
+}
+
+func TestServerPipelining(t *testing.T) {
+	network := "tcp"
+	addr := ":9999"
+	codec := "json"
+	server := NewServer()
+	server.SetPipelining(true)
+	err := server.Register(new(service.Arith))
+	if err != nil {
+		t.Error(err)
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		server.Listen(network, addr, codec)
+	}()
+	time.Sleep(time.Millisecond * 10)
+	conn, err := Dial(network, addr, codec)
+	if err != nil {
+		t.Error(err)
+	}
+	err = conn.Ping()
+	if err != nil {
+		t.Error(err)
+	}
+
+	A := int32(4)
+	B := int32(8)
+	req := &service.ArithRequest{A: A, B: B}
+	var res service.ArithResponse
+	if err := conn.Call("Arith.Multiply", req, &res); err != nil {
+		t.Error(err)
+	}
+	if res.Pro != A*B {
+		t.Error(res.Pro)
+	}
+	conn.Close()
+	server.Close()
+	wg.Wait()
+}
+
+func TestServerPollPipelining(t *testing.T) {
+	network := "tcp"
+	addr := ":9999"
+	codec := "json"
+	SetPoll(true)
+	SetPipelining(true)
+	err := RegisterName("Arith", new(service.Arith))
+	if err != nil {
+		t.Error(err)
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		Listen(network, addr, codec)
+	}()
+	time.Sleep(time.Millisecond * 10)
+	conn, err := Dial(network, addr, codec)
+	if err != nil {
+		t.Error(err)
+	}
+	err = conn.Ping()
+	if err != nil {
+		t.Error(err)
+	}
+
+	A := int32(4)
+	B := int32(8)
+	req := &service.ArithRequest{A: A, B: B}
+	var res service.ArithResponse
+	if err := conn.Call("Arith.Multiply", req, &res); err != nil {
+		t.Error(err)
+	}
+	if res.Pro != A*B {
+		t.Error(res.Pro)
+	}
+	conn.Close()
+	DefaultServer.Close()
+	wg.Wait()
+}
