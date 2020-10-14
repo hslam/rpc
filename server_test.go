d9ca540c238d521baaa59d82fc1d629d23cd4e83
hslam <791874158@qq.com>
2020-10-14 20:32:06 +0800

    add server push test

--- server_test.go
+++ server_test.go
@@ -129,3 +129,67 @@ func TestServerPollPipelining(t *testing.T) {
 	DefaultServer.Close()
 	wg.Wait()
 }
+
+func TestServerPush(t *testing.T) {
+	network := "tcp"
+	addr := ":9999"
+	codec := "json"
+	var lock sync.Mutex
+	var k = "foo"
+	var str = "bar"
+	var v = []byte(str)
+	PushFunc(func(key string) (value []byte, ok bool) {
+		if key == k {
+			lock.Lock()
+			value = v
+			lock.Unlock()
+			return value, true
+		}
+		return nil, false
+	})
+	wg := sync.WaitGroup{}
+	done := make(chan struct{}, 1)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ticker := time.NewTicker(time.Millisecond * 100)
+		for {
+			select {
+			case <-ticker.C:
+				lock.Lock()
+				v = []byte(str)
+				value := v
+				lock.Unlock()
+				Push(k, value)
+			case <-done:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		Listen(network, addr, codec)
+	}()
+	time.Sleep(time.Millisecond * 10)
+	conn, err := Dial(network, addr, codec)
+	if err != nil {
+		t.Error(err)
+	}
+	watch := conn.Watch(k)
+	for i := 0; i < 3; i++ {
+		value, err := watch.Wait()
+		if err != nil {
+			panic(err)
+		}
+		if string(value) != str {
+			t.Errorf("Watch foo:%s\n", string(value))
+		}
+	}
+	watch.Stop()
+	conn.Close()
+	close(done)
+	DefaultServer.Close()
+	wg.Wait()
+}
