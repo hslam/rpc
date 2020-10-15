7dab407b71a4eae0f779ade3be6d899fbcdf26ca
hslam <791874158@qq.com>
2020-10-15 16:46:43 +0800

    update dialer test

--- dialer_test.go
+++ dialer_test.go
@@ -1,8 +1,11 @@
 package rpc
 
 import (
+	"github.com/hslam/rpc/examples/codec/json/service"
 	"github.com/hslam/socket"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestDial(t *testing.T) {
@@ -34,13 +37,41 @@ func TestDialTLS(t *testing.T) {
 func TestDialWithOptions(t *testing.T) {
 	addr := ":9999"
 	opts := &Options{}
-	opts.Codec = ""
-	opts.Network = ""
-	if _, err := DialWithOptions(addr, opts); err == nil {
-		t.Error("should error")
-	}
 	opts.Network = "tcp"
+	opts.Codec = "json"
+	err := Register(new(service.Arith))
+	if err != nil {
+		t.Error(err)
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ListenWithOptions(addr, opts)
+	}()
+	time.Sleep(time.Millisecond * 10)
+	opts.Network = ""
+	opts.Codec = ""
 	if _, err := DialWithOptions(addr, opts); err == nil {
 		t.Error("should error")
 	}
+	opts.Codec = "json"
+	if _, err := DialWithOptions(addr, opts); err == nil {
+		t.Error("should error")
+	}
+	opts.Codec = ""
+	opts.NewCodec = NewCodec("json")
+	opts.NewSocket = NewSocket("tcp")
+	if _, err := DialWithOptions(addr, opts); err != nil {
+		t.Error(err)
+	}
+	if _, err := DialWithOptions(addr, opts); err != nil {
+		t.Error(err)
+	}
+	opts.NewHeaderEncoder = NewHeaderEncoder("json")
+	if _, err := DialWithOptions(addr, opts); err != nil {
+		t.Error(err)
+	}
+	DefaultServer.Close()
+	wg.Wait()
 }
