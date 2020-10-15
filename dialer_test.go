385b7e35317d9cb91f311cef81e5f3cdbb0a162a
hslam <791874158@qq.com>
2020-10-15 16:24:22 +0800

    update dialer test

--- dialer_test.go
+++ dialer_test.go
@@ -1,93 +1,46 @@
 package rpc
 
 import (
-	"github.com/hslam/rpc/examples/codec/json/service"
 	"github.com/hslam/socket"
-	"sync"
 	"testing"
-	"time"
 )
 
 func TestDial(t *testing.T) {
-	network := "tcp"
 	addr := ":8880"
-	codec := "json"
-	err := Register(new(service.Arith))
-	if err != nil {
-		t.Error(err)
-	}
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		Listen(network, addr, codec)
-	}()
-	time.Sleep(time.Millisecond * 10)
-	network = ""
+	network := ""
+	codec := ""
 	if _, err := Dial(network, addr, codec); err == nil {
-		t.Error(err)
+		t.Error("should error")
 	}
-	codec = ""
-	if _, err = Dial(network, addr, codec); err == nil {
-		t.Error(err)
+	network = "tcp"
+	if _, err := Dial(network, addr, codec); err == nil {
+		t.Error("should error")
 	}
-	DefaultServer.Close()
-	wg.Wait()
 }
 
 func TestDialTLS(t *testing.T) {
-	network := "tcp"
 	addr := ":8880"
-	codec := "json"
-	err := Register(new(service.Arith))
-	if err != nil {
-		t.Error(err)
-	}
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		ListenTLS(network, addr, codec, socket.DefalutTLSConfig())
-	}()
-	time.Sleep(time.Millisecond * 10)
-	network = ""
+	network := ""
+	codec := ""
 	if _, err := DialTLS(network, addr, codec, socket.SkipVerifyTLSConfig()); err == nil {
-		t.Error(err)
+		t.Error("should error")
 	}
-	codec = ""
-	if _, err = DialTLS(network, addr, codec, socket.SkipVerifyTLSConfig()); err == nil {
-		t.Error(err)
+	network = "tcp"
+	if _, err := DialTLS(network, addr, codec, socket.SkipVerifyTLSConfig()); err == nil {
+		t.Error("should error")
 	}
-	DefaultServer.Close()
-	wg.Wait()
 }
 
 func TestDialWithOptions(t *testing.T) {
-	network := "tcp"
 	addr := ":9999"
-	codec := "json"
 	opts := &Options{}
-	opts.Network = network
-	opts.Codec = codec
-	err := Register(new(service.Arith))
-	if err != nil {
-		t.Error(err)
-	}
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		ListenWithOptions(addr, opts)
-	}()
-	time.Sleep(time.Millisecond * 10)
+	opts.Codec = ""
 	opts.Network = ""
 	if _, err := DialWithOptions(addr, opts); err == nil {
-		t.Error(err)
+		t.Error("should error")
 	}
-	opts.Codec = ""
-	if _, err = DialWithOptions(addr, opts); err == nil {
-		t.Error(err)
+	opts.Network = "tcp"
+	if _, err := DialWithOptions(addr, opts); err == nil {
+		t.Error("should error")
 	}
-	DefaultServer.Close()
-	wg.Wait()
 }
