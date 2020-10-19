f58dabb4c9e2195783c63b64417826d09ebe9347
hslam <791874158@qq.com>
2020-10-19 13:14:55 +0800

    update watcher wait

--- watcher_test.go
+++ watcher_test.go
@@ -9,3 +9,11 @@ func TestWatcherTrigger(t *testing.T) {
 	watcher.trigger([]byte{}, nil)
 	watcher.trigger([]byte{}, nil)
 }
+
+func TestWatcherWait(t *testing.T) {
+	watcher := &watcher{C: make(chan *watcher, 1), done: make(chan struct{}, 1)}
+	go func() {
+		close(watcher.done)
+	}()
+	watcher.Wait()
+}
