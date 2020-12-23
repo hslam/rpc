1efc7cfed332a2474ee62ee53c2f39e1a17df3ad
hslam <791874158@qq.com>
2020-12-23 14:50:04 +0800

    update reverse proxy test

--- reverseproxy_test.go
+++ reverseproxy_test.go
@@ -36,7 +36,7 @@ func TestReverseProxy(t *testing.T) {
 		IdleConnTimeout:     time.Second * 60,
 		Options:             opts,
 	}
-	proxy := NewSingleHostReverseProxy(addr)
+	proxy := NewReverseProxy(addr)
 	proxy.Transport = trans
 	err = proxy.Ping()
 	if err != nil {
@@ -76,13 +76,126 @@ func TestReverseProxy(t *testing.T) {
 	wg.Wait()
 }
 
+func TestReverseProxyLeastTime(t *testing.T) {
+	network := "tcp"
+	addr := ":9999"
+	codec := "json"
+	opts := DefaultOptions()
+	opts.Network = network
+	opts.Codec = codec
+	server := NewServer()
+	err := server.Register(new(service.Arith))
+	if err != nil {
+		t.Error(err)
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		server.ListenWithOptions(addr, opts)
+	}()
+	time.Sleep(time.Millisecond * 10)
+	trans := &Transport{
+		MaxConnsPerHost:     1,
+		MaxIdleConnsPerHost: 1,
+		KeepAlive:           time.Second * 60,
+		IdleConnTimeout:     time.Second * 60,
+		Options:             opts,
+	}
+	proxy := NewReverseProxy(addr)
+	proxy.Select = LeastTime
+	proxy.Transport = trans
+	err = proxy.Ping()
+	if err != nil {
+		t.Error(err)
+	}
+
+	A := int32(4)
+	B := int32(8)
+	req := &service.ArithRequest{A: A, B: B}
+	var res service.ArithResponse
+	if err := proxy.Call("Arith.Multiply", req, &res); err != nil {
+		t.Error(err)
+	}
+	if res.Pro != A*B {
+		t.Error(res.Pro)
+	}
+
+	res = service.ArithResponse{}
+	call := proxy.Go("Arith.Multiply", req, &res, make(chan *Call, 1))
+	<-call.Done
+	if res.Pro != A*B {
+		t.Error(res.Pro)
+	}
+
+	call = new(Call)
+	call.ServiceMethod = "Arith.Multiply"
+	call.Args = req
+	call.Reply = &service.ArithResponse{}
+	proxy.RoundTrip(call)
+	<-call.Done
+	if res.Pro != A*B {
+		t.Error(res.Pro)
+	}
+
+	trans.Close()
+	server.Close()
+	wg.Wait()
+}
+
+func TestReverseProxyEmptyTargets(t *testing.T) {
+	defer func() {
+		if e := recover(); e == nil {
+			t.Error("should panic")
+		}
+	}()
+	NewReverseProxy()
+}
+
 func TestReverseProxyTransport(t *testing.T) {
 	addr := ":9999"
-	proxy := NewSingleHostReverseProxy(addr)
+	proxy := NewReverseProxy(addr)
 	defer func() {
 		if e := recover(); e == nil {
 			t.Error("should panic")
 		}
 	}()
 	proxy.transport()
+}
+
+func TestReverseProxyTarget(t *testing.T) {
+	proxy := NewReverseProxy(":9999", ":9998", ":9997")
+	proxy.Select = RoundRobin
+	if proxy.target() == nil {
+		t.Error()
+	}
+	proxy.Select = Random
+	if proxy.target() == nil {
+		t.Error()
+	}
+	proxy.Select = LeastTime
+	if proxy.target() == nil {
+		t.Error()
+	}
+	if proxy.target() == nil {
+		t.Error()
+	}
+	proxy = &ReverseProxy{}
+	defer func() {
+		if e := recover(); e == nil {
+			t.Error("should panic")
+		}
+	}()
+	proxy.target()
+}
+
+func TestTopK(t *testing.T) {
+	l := list{&target{score: 10}, &target{score: 7}, &target{score: 2}, &target{score: 5}, &target{score: 1}, &target{score: 6}}
+	minHeap(l)
+	n := l.Len()
+	for i := 1; i < n; i++ {
+		if l.Less(i, 0) {
+			t.Error("heap error")
+		}
+	}
 }
